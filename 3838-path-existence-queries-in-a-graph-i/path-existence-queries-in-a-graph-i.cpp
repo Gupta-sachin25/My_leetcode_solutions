b49e19bcd7631ba7927e9556b696e82d3f3a5518
Gupta-sachin25 <sachgupta2509@gmail.com>
2026-09-04 10:39:10 +0530

    Time: 8 ms (63.77%) | Memory: 236.1 MB (14.04%) - LeetSync

--- 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.cpp
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.cpp
@@ -1,17 +1,47 @@
 class Solution {
 public:
+vector<int> par;
+vector<int> rank;
+
+
     vector<bool> pathExistenceQueries(int n, vector<int>& nums, int k, vector<vector<int>>& q) {
-        int id=1;
-        vector<int> temp(n,1);
-        for(int i=1;i<n;i++){
-            if(nums[i]-nums[i-1]>k) id++;
-            temp[i]=id;
-        }
-        vector<bool> ans(q.size() , false);
+
+    par.resize(n);
+    rank.resize(n);
+    for(int i=0;i<n;i++) par[i]=i;
+    for(int i=0;i<n;i++) rank[i]=1;
+
+    for(int i=1;i<n;i++){
+        if(nums[i]-nums[i-1]<=k) par[i]=par[i-1];
+    }
+
+    vector<bool> ans(q.size() , false);
             for(int i=0;i<q.size();i++){
-if(temp[q[i][0]] == temp[q[i][1]])
+if(par[q[i][0]] == par[q[i][1]])
     ans[i] = true;
             }
             return ans;
+
+
+
+
+
+
+
+
+
+        
+//         int id=1;
+//         vector<int> temp(n,1);
+//         for(int i=1;i<n;i++){
+//             if(nums[i]-nums[i-1]>k) id++;
+//             temp[i]=id;
+//         }
+//         vector<bool> ans(q.size() , false);
+//             for(int i=0;i<q.size();i++){
+// if(temp[q[i][0]] == temp[q[i][1]])
+//     ans[i] = true;
+//             }
+//             return ans;
     }
 };
